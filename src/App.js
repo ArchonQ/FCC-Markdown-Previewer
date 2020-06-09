25bcc086f3250462b82618ddfce6c5702b8c50c9
brandon quezada <brandon.cypaso@gmail.com>
2020-06-09 14:56:32 -0500

    bootstrap added, basic structure laid

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+// import { container, row, col } from 'react-bootstrap';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-	return (
-		<div className='App'>
-			<p>this is sparta baby!</p>
-		</div>
-	);
+class App extends React.Component {
+	state = {
+		markdown: '',
+	};
+
+	handleChange = (e) => {
+		this.setState({
+			markdown: e.target.value,
+		});
+	};
+	render() {
+		const { markdown } = this.state;
+
+		return (
+			<div className='App'>
+				<div className='container'>
+					<h2 className='text-center mt-4'>Convert your Markdown</h2>
+					<div className='row'>
+						<div className='col-6'>
+							<textarea
+								id='editor'
+								value={markdown}
+								onChange={this.handleChange}
+							/>
+						</div>
+						<div className='col-6 preview' id='preview'>
+							<p>hello</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
